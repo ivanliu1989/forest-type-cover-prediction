eb5201db2925a6f1b196d44d00dadfbc2355dd0f
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-10 16:28:13 +1000

    rpart model

--- testFile.R
+++ testFile.R
@@ -5,6 +5,7 @@
 # Load data
     system.time(train <- fread(dir()[6]))
     system.time(test <- fread(dir()[3]))
+    test$Cover_Type <- NA
     sample <- fread(dir()[1])
 # Preprocessing data
     str(train)
@@ -19,13 +20,14 @@
     fc <- findLinearCombos(train)
     fc
 # Plotting data
-    plot(train$Cover_Type,train$Elevation)
+    boxplot(train$Cover_Type,train$Elevation)
 # Train the model
     fit1 <- train(Cover_Type~., method="rpart",data = train)
     fit2 <- train(Cover_Type~., method="rf",data = train)
     fit3 <- svm()
     summary(fit1)
 # Predict model
-    pred1 <- predict(fit1,train)
-    pred1
-    train$Cover_Type
+    pred1 <- predict(fit1,test)
+    pred1.output <- data.frame(Id=test$Id, Cover_Type=pred1, rownames=F)
+    head(pred1)
+
